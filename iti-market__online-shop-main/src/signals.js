c1e5c45d7c8df8218d1a3b132337c39b1a25a26f
Đinh Quốc Cường <tr6r20@gmail.com>
2025-09-20 16:40:48 +0700

    add global variables

--- iti-market__online-shop-main/src/signals.js
+++ iti-market__online-shop-main/src/signals.js
@@ -2,4 +2,8 @@ import { signal } from "@preact/signals-react";
 
 // Biến signal toàn cục
 export const authSignal = signal(false);
+export const Cart = signal([]);
+export const Product = signal([]);
+
+
 // authSignal.value sẽ là trạng thái hiện tại
