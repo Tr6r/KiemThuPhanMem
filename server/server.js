673c07ed11dc758bb2245ff115fdeb1a0673b3c9
Đinh Quốc Cường <tr6r20@gmail.com>
2025-09-20 16:07:19 +0700

    add import

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
+require('dotenv').config();
+
 var express = require('express');
 var bodyParser = require("body-parser");
 var mysql = require('mysql2');
+
 const cors = require('cors');
 var app = express();
 app.use(cors());
